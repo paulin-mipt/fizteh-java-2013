6c599fa7890526be223c6e596cc374923dfaa6a4
Kamilot <musakami@gmail.com>
2013-12-18 11:15:53 +0400

    one more file

--- src/ru/fizteh/fivt/students/musin/filemap/ShellClientDatabaseHandler.java
+++ src/ru/fizteh/fivt/students/musin/filemap/ShellClientDatabaseHandler.java
@@ -343,8 +343,8 @@ public class ShellClientDatabaseHandler {
                     }
                     Storeable value = current.remove(args.get(0));
                     if (value != null) {
-                        shell.writer.printf("removed%s%s%s",
-                                System.lineSeparator(), database.serialize(current, value), System.lineSeparator());
+                        shell.writer.printf("removed%s",
+                                System.lineSeparator(), System.lineSeparator());
                     } else {
                         shell.writer.println("not found");
                     }
