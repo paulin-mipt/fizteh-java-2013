c3bc7177fe534f45d7c85c7e006c8b2dd0108de4
paulin-mipt <paulin.mipt@gmail.com>
2013-12-26 13:52:40 +0400

    exit exceptions

--- src/ru/fizteh/fivt/students/paulinMatavina/filemap/MultiFileMapMain.java
+++ src/ru/fizteh/fivt/students/paulinMatavina/filemap/MultiFileMapMain.java
@@ -4,12 +4,22 @@ import ru.fizteh.fivt.students.paulinMatavina.servlet.MyServer;
 import ru.fizteh.fivt.students.paulinMatavina.utils.CommandRunner;
 
 public class MultiFileMapMain {
+    private static void tryToStop(MyServer server) {
+        try {
+            server.stop();
+        } catch (Throwable e2) {
+            //ignore
+        }
+    }
+    
     public static void main(String[] args) {
         final MyServer server;
         MyTableProvider state = null;
         try {
             String property = System.getProperty("fizteh.db.dir");
             state = new MyTableProvider(property);  
+        } catch (DbExitException e) {
+            System.exit(Integer.parseInt(e.getMessage()));
         } catch (Throwable e) {
             System.out.println(e.getMessage());
             System.exit(1);
@@ -21,19 +31,14 @@ public class MultiFileMapMain {
             Runtime.getRuntime().addShutdownHook(new Thread() {
                 @Override
                 public void run() {
-                    try {
-                        server.stop();
-                    } catch (Throwable e) {
-                        //ignore
-                    }
+                    tryToStop(server);
                 }
             });
-        } catch (Throwable e) {
-            try {
-                server.stop();
-            } catch (Throwable e2) {
-                //ignore
-            }
+        } catch (DbExitException e) {
+            tryToStop(server);
+            System.exit(Integer.parseInt(e.getMessage()));
+        }  catch (Throwable e) {
+            tryToStop(server);
             System.out.println(e.getMessage());
             System.exit(1);
         }
